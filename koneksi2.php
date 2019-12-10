737fc27cea156452047e25d810f9d1f53c363658
Rizky Oktaliana <rizkyoktaliana.ti10.poliwangi@gmail.com>
2019-12-10 11:34:10 +0700

    koneksi database

--- koneksi2.php
+++ koneksi2.php
@@ -1,6 +1,7 @@
-<!-- <?php
+ <?php
 $host = "localhost"; // Nama hostnya
 $user = "root"; // Username
 $pass = ""; // Password (Isi jika menggunakan password)
 $connect = mysqli_connect($host, $user, $pass, "apk_berita"); // Koneksi ke MySQL
-?> -->
+?>
+ 
